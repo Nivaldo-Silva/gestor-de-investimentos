b767654e6f847c35082bef923a85861d03058d55
Nivaldo Silva <nivaldosilva.contato@gmail.com>
2025-02-28 22:29:33 -0300

    feat: initial project setup

---
+++ src/main/java/io/github/nivaldosilva/investment_manager_api/InvestmentManagerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.nivaldosilva.investment_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nivaldosilva/investment_manager_api/InvestmentManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nivaldosilva.investment_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
